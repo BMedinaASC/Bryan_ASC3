03c4357b49257572f43143b06c97c8adde1e9350
BMedinaASC <bryanmedina8200@gmail.com>
2016-08-02 11:17:13 -0700

    pick a game project

--- All Stario bros/javaScript/main.js
+++ All Stario bros/javaScript/main.js
@@ -1,20 +1,57 @@
-var space;
+var floor;
+var player;
 
 function preload()
 {
+	floor = createSprite(0, 550, 2048, 30);
+	floor.shapeColor = color(139,69,19);
 
+	//var obstacle1 = 
+	player = createSprite(20, 410, 20, 40);
+	player.shapeColor = color(0, 255, 255);	
 }
 
 function setup()
 {
-	space = loadImage("spaceBackground.jpg");
 	createCanvas(1024, 600);
+}
+
+
+function keyPressed()
+{
+	
+
+	if (key === 'd')
+	{
+		 player.velocity.x += 2;
+	}
+
+	if (key === 'a')
+	{
+		player.velocity.x -= 2;
+	}
+
+	if (keyCode === '32')
+	{
+		player.velocity.y -= 4;
+	}
 }
 
 function draw()
 {
 	background(120, 0, 120);
-	var floor = createSprite(10, 550, 2048, 30);
-	floor.shapeColor = color(139,69,19);
+	
+	player.velocity.y += 2;
+	
+	if (player.collide(floor))
+		{
+			player.velocity.y = 0;
+		}
+	/*else
+	{
+		player.velocity.x = 0;
+	}*/
+
+	//gravity();
 	drawSprites();
 }
